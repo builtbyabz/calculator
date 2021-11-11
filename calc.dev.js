176d25187e312aa658322383bad4daaf4bf90dde
Basit Oketunde <basitoketunde@Basits-MacBook-Pro.local>
2021-11-11 22:34:12 +0000

    push

--- calc.dev.js
+++ calc.dev.js
@@ -22,31 +22,56 @@ var equalusButton = document.querySelector(".equals");
 console.log(operators);
 var firstNumber = "";
 var secondNumber = "";
-var operator = "";
+var operator = ""; //added empty array to 
+
+var numbers1 = [];
 
 for (i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", function (event) {
     firstNumber = event.target.value;
-    output.innerHTML += firstNumber; // console.log(firstNumber);
+    output.innerHTML += firstNumber;
+    console.log(firstNumber);
   });
 }
 
 for (i = 0; i < operators.length; i++) {
   operators[i].addEventListener("click", function (event) {
-    operator = event.target.innerText;
-    secondNumber = firstNumber;
-    firstNumber = "";
-    console.log(operator); //output.innerHTML += firstNumber;
+    firstNumber = output.innerHTML;
+    numbers1.push(firstNumber);
+    operator = event.target.innerText; //secondNumber = firstNumber;
+    //firstNumber = "";
+
+    console.log(operator);
+    output.innerHTML += operator;
+    console.log(numbers1);
+    output.innerHTML = "";
   });
 }
 
-equalusButton.addEventListener("click", function () {
-  console.log(addNumbers(firstNumber, secondNumber)); // console.log(firstNumber, secondNumber, operator);
-});
-
-var addNumbers = function addNumbers(first, second) {
-  return first + second;
+var calculateNumbers = function calculateNumbers(first, second) {
+  if (operator === "+") {
+    console.log(first);
+    console.log(second);
+    return parseInt(first) + parseInt(second);
+  } else if (operator === "-") {
+    return parseInt(first) - parseInt(second);
+  } else if (operator === "÷") {
+    return parseInt(first) / parseInt(second);
+  } else if (operator === "x") {
+    return parseInt(first) * parseInt(second);
+  }
 };
+
+equalusButton.addEventListener("click", function () {
+  secondNumber = output.innerHTML;
+  numbers1.push(secondNumber);
+  console.log(numbers1);
+  output.innerHTML = calculateNumbers(numbers1[0], numbers1[1]); //secondNumber = numbers1[1]
+  //console.log(secondNumber)
+  //console.log(calculateNumbers(numbers1[0], numbers1[1]))
+  //console.log(calculateNumbers(firstNumber, secondNumber))
+  // console.log(firstNumber, secondNumber, operator);
+});
 /* 1. Get the elements I neeed from the DOM.
    2. Store the first number after selecting operand.
 
--- calc.js
+++ calc.js
@@ -29,35 +29,71 @@ let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 
+//added empty array to 
+let numbers1 = []
+
 for (i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", (event) => {
         firstNumber = event.target.value;
         output.innerHTML += firstNumber;
-        // console.log(firstNumber);
+        console.log(firstNumber);
     });
 }
 
 for (i = 0; i < operators.length; i++) {
     operators[i].addEventListener("click", (event) => {
+        firstNumber = output.innerHTML
+        numbers1.push(firstNumber)
         operator = event.target.innerText;
-        secondNumber = firstNumber;
-        firstNumber = "";
+        //secondNumber = firstNumber;
+        //firstNumber = "";
         console.log(operator);
-        //output.innerHTML += firstNumber;
+        output.innerHTML += operator;
+        console.log(numbers1)
+        output.innerHTML = "";
     });
 }
 
+
+
+const calculateNumbers = (first, second) => {
+
+
+    if (operator === "+") {
+        console.log(first);
+        console.log(second);
+        return parseInt(first) + parseInt(second);
+    } else if (operator === "-") {
+        return parseInt(first) - parseInt(second);
+    } else if (operator === "÷") {
+        return parseInt(first) / parseInt(second);
+    } else if (operator === "x") {
+        return parseInt(first) * parseInt(second);
+    }
+
+}
+
+
+
+
 equalusButton.addEventListener("click", () => {
-     console.log(addNumbers(firstNumber, secondNumber));
+    secondNumber = output.innerHTML
+    numbers1.push(secondNumber)
+
+    console.log(numbers1)
+    output.innerHTML = (calculateNumbers(numbers1[0], numbers1[1]))
+
+    
+
+    //secondNumber = numbers1[1]
+    //console.log(secondNumber)
+    //console.log(calculateNumbers(numbers1[0], numbers1[1]))
+    //console.log(calculateNumbers(firstNumber, secondNumber))
 
     // console.log(firstNumber, secondNumber, operator);
 });
 
-const addNumbers = (first, second) => {
 
-    return first + second;
-
-}
 
 /* 1. Get the elements I neeed from the DOM.
    2. Store the first number after selecting operand.
